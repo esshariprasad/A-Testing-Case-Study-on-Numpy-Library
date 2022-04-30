d9db75a95cb71ab8ef3105d5c52799b7ea34bdce
sai shiva hari prasad <esshariprasad@gmail.com>
2022-04-30 18:22:35 -0500

    added main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import re
+from assertndebug import *
+from assert_statement_agreegation import *
+
+
+
+
+if __name__=="__main__":
+    Generate_assertndebugCSV()
+    Generate_assert_aggreagationCSV()
